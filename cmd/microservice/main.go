402e7bf8799db7f2829b7d856c61ae316471e12b
Matiss Kiris <matiss.kiris@gmail.com>
2020-05-14 17:11:58 +0300

    Add CLI commands

--- cmd/microservice/main.go
+++ cmd/microservice/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/matiss/go-microservice-test/services"
 )
@@ -10,48 +12,24 @@ const (
 	feedURL = "https://www.bank.lv/vk/ecb_rss.xml"
 )
 
-func init() {
+// Setup database tables
+func setup(mysqlService *services.MySQLService) error {
+	fmt.Println("Running setup...")
+	err := mysqlService.Setup()
+	if err != nil {
+		return err
+	}
 
+	fmt.Println("Done!")
+	return nil
 }
 
-func main() {
-	// Setup MySQL service
-	mysqlService, err := services.NewMySQLService("root", "12345678", "tcp(127.0.0.1:3306)", "go_microservice_test")
-	if err != nil {
-		panic(err)
-	}
-
-	// Run setup
-	// TODO: Move this into seperate command
-	err = mysqlService.Setup()
-	if err != nil {
-		panic(err)
-	}
-
-	// Setup currency feed service
-	currencyFeedService := services.NewCurrencyFeedService(mysqlService, feedURL)
-
-	// Setup currency service
-	currencyService := services.NewCurrencyService(mysqlService)
-
-	currencies, err := currencyService.BySymbol("PHP", 20)
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Printf("Values (PHP): %+v\n", currencies)
-
-	currencies, err = currencyService.Latest()
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Printf("Latest currencies: %+v\n", currencies)
-
+// Update currencies
+func update(mysqlService *services.MySQLService, currencyFeedService *services.CurrencyFeedService) error {
 	// Fetch and parse currency feed
-	log.Println("Fetching latest currency feed")
+	fmt.Println("Fetching latest currency feed")
 
-	err = currencyFeedService.Fetch()
+	err := currencyFeedService.Fetch()
 	if err != nil {
 		log.Println(err)
 	}
@@ -69,11 +47,85 @@ func main() {
 			panic(err)
 		}
 
-		log.Println("Currencies updated successfully!")
+		fmt.Println("Currencies updated successfully!")
 	} else {
-		log.Println("No need to update the currencies")
+		fmt.Println("No need to update!")
+	}
+
+	return nil
+}
+
+func serve(mysqlService *services.MySQLService, currencyService *services.CurrencyService) error {
+	fmt.Println("Starting HTTP server...")
+
+	currencies, err := currencyService.BySymbol("PHP", 20)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Printf("Values (PHP): %+v\n", currencies)
+
+	currencies, err = currencyService.Latest()
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Printf("Latest currencies: %+v\n", currencies)
+
+	return nil
+}
+
+const infoText = `
+Go Microservice Test
+
+Usage:
+  go-microservice [command]
+
+Available Commands:
+  update Fetch and update latest currencies
+  serve Start HTTP server
+  setup Setup database tables
+
+`
+
+func printInfo() {
+	fmt.Println(infoText)
+}
+
+func main() {
+	// Print info text
+	if len(os.Args) < 2 {
+		printInfo()
+		return
+	}
+
+	// Setup MySQL service
+	mysqlService, err := services.NewMySQLService("root", "12345678", "tcp(127.0.0.1:3306)", "go_microservice_test")
+	if err != nil {
+		panic(err)
 	}
 
 	// Terminate database service before exiting
-	mysqlService.Close()
+	defer mysqlService.Close()
+
+	// Setup currency feed service
+	currencyFeedService := services.NewCurrencyFeedService(mysqlService, feedURL)
+
+	// Setup currency service
+	currencyService := services.NewCurrencyService(mysqlService)
+
+	switch os.Args[1] {
+	case "update":
+		// Run update command
+		update(mysqlService, currencyFeedService)
+	case "serve":
+		// Run serve command
+		serve(mysqlService, currencyService)
+	case "setup":
+		// Run setup command
+		setup(mysqlService)
+	default:
+		printInfo()
+	}
+
 }
